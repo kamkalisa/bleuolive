330a18270eed3e96d28353285096a2ed1eda7ea7
Genisha <genishamorton@icloud.com>
2021-03-12 18:52:09 -0600

    testing code added java script bug

--- lab6.js
+++ lab6.js
@@ -1,2 +1,11 @@
+let myname;
 console.log("BLEU OLIVE");
-    
+myname="Genisha Morton";
+//console.log(myname);
+myname=prompt("what is your name?");
+console.log(myname);
+let title=prompt("enter a title");
+document.getElementById("title").innerHTML=title;
+let email=prompt("what is your email?");
+console.log(email);
+document.getElementById("myemail").innerHTML=email;
