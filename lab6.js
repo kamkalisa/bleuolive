2f6a0f1a1a20933b203fd887e9acd0ae7ce7ab2e
Genisha <genishamorton@icloud.com>
2021-03-12 21:04:09 -0600

    added functions to website

--- lab6.js
+++ lab6.js
@@ -4,11 +4,26 @@ myname="Genisha Morton";
 //console.log(myname);
 myname=prompt("what is your name?");
 console.log(myname);
-let title=prompt("enter a title");
-document.getElementById("title").innerHTML=title;
-let email=prompt("what is your email?");
-console.log(email);
-document.getElementById("myemail").innerHTML=email;
-let age=prompt("what is your age?");
-if (age<13) {alert("Get Parental Consent");}
 
+
+function permission() {
+    let age=prompt("what is your age?");
+    
+    if (age<13) {
+        alert("Get Parental Consent");
+    }
+    
+}
+function contact() {
+    let email=prompt("what is your email?");
+
+    console.log(email);
+    document.getElementById("myemail").innerHTML=email;
+    
+}
+function title() {
+    let title=prompt("enter a title");
+
+    document.getElementById("title").innerHTML=title;
+
+}
